e4e7caac1a01d398bbb9d6210a04333ed22e55c0
Beastmaster69420 <88301082+Beastmaster69420@users.noreply.github.com>
2023-07-01 19:32:26 +0545

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import time
 
-app = Flask(__name__)
-
-@app.route('/')
+app = Flask(__name__, template_folder="./")
+todo_list =[]
+@app.route("/")
 def home():
-    return 'Hello, World!'
+    return render_template("index.html",todos=todo_list)
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['item']
+    if len(task) > 0:
+        id = int(time.time()*1000)
+        todo_list.append({'id': id, 'task': task})
+        print(todo_list)
+
+    return redirect(url_for('home'))
+@app.route('/<int:id>/delete', methods=['POST'])
+def delete(id):
+    #id = int(id)
+    for i in range(len(todo_list)):
+        if todo_list[i]['id'] == id:
+            todo_list.pop(i)
+            break
+    return redirect(url_for('home'))
